628c7711a221b724c04456c099b1c129cf55298f
Hamza Al-Sarsour <hamze.85@live.com>
2021-05-31 20:21:02 +0300

    finish the demo

--- app/src/main/java/com/taskfoundation/androidweardemo/MainActivity.java
+++ app/src/main/java/com/taskfoundation/androidweardemo/MainActivity.java
@@ -3,13 +3,18 @@ package com.taskfoundation.androidweardemo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.taskfoundation.androidweardemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
+    private ListView listView;
     private ActivityMainBinding binding;
 
     @Override
@@ -19,12 +24,25 @@ public class MainActivity extends Activity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
+        listView = binding.listView;
 
         if (getResources().getConfiguration().isScreenRound()) {
             Log.i("Device", "Round");
         } else {
             Log.i("Device", "Square");
         }
+
+        String[] names = {"Hamza", "Ahmad", "Zaid", "Jafaar", "Yazeed"};
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, names);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "name is: " + names[position], Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
